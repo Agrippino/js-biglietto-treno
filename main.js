98a34bf7ee0f647f7d2bab7aa53643087b0de4fe
Agrippino <agrippinorussiello@outlook.it>
2022-03-22 17:14:15 +0100

    Correzione

--- main.js
+++ main.js
@@ -1,30 +1,24 @@
 let Chilometri = prompt ("Salve viaggiatore, inserisca quanti chilometri percorrerà oggi,grazie");
 let passeggeroEta = prompt ("Qual è l'eta del passeggero?");
+let prezzofinale
 
+let x = Chilometri;
+let y = 0.21;
+let z = x * y;
 
-{let x = Chilometri;
-    let y = 0.21;
-    let z = x * y;
-    let num = z;
-    let n = num.toFixed(2);
-    document.getElementById("demo").innerHTML = z;
+if(passeggeroEta < 18){
+let sconto18 = 0.2;
+let v = z * sconto18;
+prezzofinale = z -v; 
 }
-
-if(passeggeroEta < "18"){
-let z;
-let y = 20;
-let v = z % y;
-let num = v;
-let n = num.toFixed(2);}
-else if(passeggeroEta >= "65"){
-let z;
-let y = 20;
-let v = z % y;
-let num = v;
-let n = num.toFixed(2);
+else if(passeggeroEta >= 65){
+let sconto65 = 0.4;
+let v = z * sconto65;
+prezzofinale = z - v;
 }
 else 
-{ z }
+{ prezzofinale = z }
+let num = prezzofinale;
+let n = num.toFixed(2);
 
-document.getElementById("demo").innerHTML = z;
-document.getElementById("demo").innerHTML = v;
+document.getElementById("demo").innerHTML = n;
