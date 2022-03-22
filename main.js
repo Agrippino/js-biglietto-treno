ae0ffa76874564f8ae55713b984a585ad4c88258
Agrippino <agrippinorussiello@outlook.it>
2022-03-22 15:25:45 +0100

    prove JS

---
+++ main.js
@@ -0,0 +1,23 @@
+let prezzodelBiglietto = prompt ("Salve viaggiatore, inserisca quanti chilometri percorrerà oggi,grazie");
+let passeggeroEta = prompt ("Qual è l'eta del passeggero?");
+
+
+
+if(passeggeroEta < "18"){
+let z;
+let y = 20;
+let v = z % y;}
+else if(passeggeroEta >= "65"){
+let z;
+let y = 20;
+let v = z % y;
+}
+else passeggeroEta
+{
+let x = prezzodelBiglietto;
+const y = 0.21;
+let z = x * y;
+}
+
+document.getElementById("demo").innerHTML = z;
+document.getElementById("demo").innerHTML = v;
