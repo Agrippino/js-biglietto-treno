a98b74a506915638ee9c4263f54ae7378bc6342d
Agrippino <agrippinorussiello@outlook.it>
2022-03-22 16:01:42 +0100

    prova funzionamento js

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
-let prezzodelBiglietto = prompt ("Salve viaggiatore, inserisca quanti chilometri percorrerà oggi,grazie");
+let Chilometri = prompt ("Salve viaggiatore, inserisca quanti chilometri percorrerà oggi,grazie");
 let passeggeroEta = prompt ("Qual è l'eta del passeggero?");
 
 
+{let x = Chilometri;
+    let y = 0.21;
+    let z = x * y;
+    let num = z;
+    let n = num.toFixed(2);
+    document.getElementById("demo").innerHTML = z;
+}
 
 if(passeggeroEta < "18"){
 let z;
 let y = 20;
-let v = z % y;}
+let v = z % y;
+let num = v;
+let n = num.toFixed(2);}
 else if(passeggeroEta >= "65"){
 let z;
 let y = 20;
 let v = z % y;
+let num = v;
+let n = num.toFixed(2);
 }
-else passeggeroEta
-{
-let x = prezzodelBiglietto;
-const y = 0.21;
-let z = x * y;
-}
+else 
+{ z }
 
 document.getElementById("demo").innerHTML = z;
 document.getElementById("demo").innerHTML = v;
